a66359f8aeb18a1897fd28bc30b989afc229c346
Tahesha <taheshao@gmail.com>
2024-07-30 08:41:06 -0400

    Added data file so whole app could use dummy data.

--- client/src/components/Posts.jsx
+++ client/src/components/Posts.jsx
@@ -1,57 +1,22 @@
 import React, {useState} from 'react'
 
-import Thumbnail1 from '../images/blog1.jpg'
-import Thumbnail2 from '../images/blog2.jpg'
-import Thumbnail3 from '../images/blog3.jpg'
-import Thumbnail4 from '../images/blog4.jpg'
+
 import PostItem from './PostItem'
+import {DUMMY_POSTS} from '../data'
 
 
-const DUMMY_POSTS = [
-{
-    id: '1',
-    thumbnail: Thumbnail1,
-    category: 'science',
-    title: 'This is the title of the very first post.',
-    desc: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Distinctio, veritatis atque maxime excepturi dolore deleniti vel quae minus vitae fugit, tenetur tempora provident necessitatibus mollitia harum optio reprehenderit enim id.',
-    authorID: 3
-},
-{
-    id: '2',
-    thumbnail: Thumbnail2,
-    category: 'science',
-    title: 'This is the title of the very second post.',
-    desc: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Distinctio, veritatis atque maxime excepturi dolore deleniti vel quae minus vitae fugit, tenetur tempora provident necessitatibus mollitia harum optio reprehenderit enim id.',
-    authorID: 1
-},
-{
-    id: '3',
-    thumbnail: Thumbnail3,
-    category: 'weather',
-    title: 'This is the title of the very third post.',
-    desc: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Distinctio, veritatis atque maxime excepturi dolore deleniti vel quae minus vitae fugit, tenetur tempora provident necessitatibus mollitia harum optio reprehenderit enim id.',
-    authorID: 13
-},
-{
-    id: '4',
-    thumbnail: Thumbnail4,
-    category: 'farming',
-    title: 'This is the title of the very last post.',
-    desc: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Distinctio, veritatis atque maxime excepturi dolore deleniti vel quae minus vitae fugit, tenetur tempora provident necessitatibus mollitia harum optio reprehenderit enim id.',
-    authorID: 11
-},
-]
+
 
 const Posts = () => {
     const [posts, setPosts] = useState (DUMMY_POSTS)
   return (
 <section className="posts">
-    <div className="container posts_container">
+    {posts.length > 0 ? <div className="container posts_container">
               {
                   posts.map(({ id, thumbnail, category, title, desc, authorID }) =>
                       <PostItem key={id} postID={id} thumbnail={thumbnail} category={category} title={title} description={desc} authorID={authorID} />)
               }
-    </div>
+    </div> : <h2>No posts found</h2>}
 </section>  )
 }
 
---
+++ client/src/data.js
@@ -0,0 +1,39 @@
+import Thumbnail1 from './images/blog1.jpg'
+import Thumbnail2 from './images/blog2.jpg'
+import Thumbnail3 from './images/blog3.jpg'
+import Thumbnail4 from './images/blog4.jpg'
+
+export const DUMMY_POSTS = [
+    {
+        id: '1',
+        thumbnail: Thumbnail1,
+        category: 'science',
+        title: 'This is the title of the very first post.',
+        desc: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Distinctio, veritatis atque maxime excepturi dolore deleniti vel quae minus vitae fugit, tenetur tempora provident necessitatibus mollitia harum optio reprehenderit enim id.',
+        authorID: 3
+    },
+    {
+        id: '2',
+        thumbnail: Thumbnail2,
+        category: 'science',
+        title: 'This is the title of the very second post.',
+        desc: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Distinctio, veritatis atque maxime excepturi dolore deleniti vel quae minus vitae fugit, tenetur tempora provident necessitatibus mollitia harum optio reprehenderit enim id.',
+        authorID: 1
+    },
+    {
+        id: '3',
+        thumbnail: Thumbnail3,
+        category: 'weather',
+        title: 'This is the title of the very third post.',
+        desc: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Distinctio, veritatis atque maxime excepturi dolore deleniti vel quae minus vitae fugit, tenetur tempora provident necessitatibus mollitia harum optio reprehenderit enim id.',
+        authorID: 13
+    },
+    {
+        id: '4',
+        thumbnail: Thumbnail4,
+        category: 'farming',
+        title: 'This is the title of the very last post.',
+        desc: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Distinctio, veritatis atque maxime excepturi dolore deleniti vel quae minus vitae fugit, tenetur tempora provident necessitatibus mollitia harum optio reprehenderit enim id.',
+        authorID: 11
+    },
+    ]
