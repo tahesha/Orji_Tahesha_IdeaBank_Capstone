4adb5ffb93860a4439f426565ea9d007050ec99c
Tahesha <taheshao@gmail.com>
2024-07-30 12:57:01 -0400

    Completed User Profile

--- client/src/pages/UserProfile.jsx
+++ client/src/pages/UserProfile.jsx
@@ -8,7 +8,13 @@ import { FaCheck } from "react-icons/fa";
 
 
 const UserProfile = () => {
-  const [avatar, setAvatar] = useState('')
+  const [avatar, setAvatar] = useState(Avatar)
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [currentPassword, setCurrentPassword] = useState('')
+  const [newPassword, setNewPassword] = useState('')
+  const [confirmNewPassword, setConfirmNewPassword] = useState('')
+
 
 return (
 <section className="profile">
@@ -17,7 +23,7 @@ return (
     <div className="profile_details">
       <div className="avatar_wrapper">
         <div className="profile_avatar">
-          <img src={Avatar} alt="" />
+          <img src={avatar} alt="" />
         </div>
         {/*Form to update avatar*/}
         <form className="avatar_form">
@@ -28,6 +34,19 @@ return (
       </div>
 
       <h1>Ernest Achiever</h1>
+
+        {/*Form to update user details*/}
+        <form action="" className="form profile_form">
+          <p className="form_error-message">This is an error message.</p>
+          <input type="text" placeholder='Full Name' value={name} onChange={e => setName(e.target.value)} />
+          <input type="email" placeholder='Email' value={email} onChange={e => setEmail(e.target.value)} />
+          <input type="password" placeholder='Current Password' value={currentPassword} onChange={e => setCurrentPassword(e.target.value)} />
+          <input type="password" placeholder='New Password' value={newPassword} onChange={e => setNewPassword(e.target.value)} />
+          <input type="password" placeholder='Confirm New Password' value={confirmNewPassword} onChange={e => setConfirmNewPassword(e.target.value)} />
+
+<button type="submit" className='btn primary'>Update details</button>
+        </form>
+
     </div>
   </div>
 </section>  )
