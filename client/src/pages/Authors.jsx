a2035fff665d29ff36326357f011ff72fd3bd764
Tahesha <taheshao@gmail.com>
2024-07-30 10:35:10 -0400

    Added author data.

--- client/src/pages/Authors.jsx
+++ client/src/pages/Authors.jsx
@@ -1,5 +1,23 @@
 import React from 'react'
 
+
+import Avatar1 from '../images/avatar1.jpg'
+import Avatar2 from '../images/avatar2.jpg'
+import Avatar3 from '../images/avatar3.jpg'
+import Avatar4 from '../images/avatar4.jpg'
+import Avatar5 from '../images/avatar5.jpg'
+
+
+
+const authorData = [
+{id: 1, avatar: Avatar1, name: 'Ernest Achiever', posts: 3},
+{id: 2, avatar: Avatar2, name: 'Jane Doe', posts: 5},
+{id: 3, avatar: Avatar3, name: 'Dramani Mahama', posts: 0},
+{id: 4, avatar: Avatar4, name: 'Nana Addo', posts: 2},
+{id: 5, avatar: Avatar5, name: 'Hajia Bintu', posts: 1},
+]
+
+
 const Authors = () => {
   return (
     <div>Authors</div>
