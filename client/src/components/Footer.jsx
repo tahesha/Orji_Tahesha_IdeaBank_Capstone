94cb07d779a1a067de77ada3eeef0acc4b79ab96
Tahesha <taheshao@gmail.com>
2024-07-29 23:50:01 -0400

    Added Footer info.

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+<footer>
+<ul className="footer_categories">
+
+    <li><Link to="/posts/categories/Agriculture">Agriculture</Link></li>
+    <li><Link to="/posts/categories/Business">Business</Link></li>
+    <li><Link to="/posts/categories/Education">Education</Link></li>
+    <li><Link to="/posts/categories/Entertainment">Entertainment</Link></li>
+    <li><Link to="/posts/categories/Art">Art</Link></li>
+    <li><Link to="/posts/categories/Investment">Investment</Link></li>
+    <li><Link to="/posts/categories/Uncategorized">Uncategorized</Link></li>
+    <li><Link to="/posts/categories/Weather">Weather</Link></li>
+  <div className='footer_copyright'>
+    <small>ALl Rights Reserved &copy; Copyright, Tahesha Orji</small>
+  </div>
+</ul>
+
+</footer> 
   )
 }
 
